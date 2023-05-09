812f4bb83051b8fb91707797112348774129f89b
Dom <46989387+dvitelli@users.noreply.github.com>
2023-05-09 12:35:43 -0700

    Create ForEachLoop.cs

---
+++ ForEachLoop.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+namespace ExampleProject
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            foreach(char s in "abc")
+            {
+                System.Diagnostics.Debug.WriteLine(s);
+            }
+                
+            
+        }
+    }
+}

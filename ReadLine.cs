94adba06b9c5cc0f18d49d4fba904d1867ed28e4
Dom <46989387+dvitelli@users.noreply.github.com>
2023-05-09 14:18:45 -0700

    Create ReadLine.cs

---
+++ ReadLine.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+namespace ExampleProject
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+
+            Console.WriteLine("Type some text: ");
+            string userInput = Console.ReadLine();
+            System.Diagnostics.Debug.WriteLine(userInput);
+            
+        }
+    }
+}
